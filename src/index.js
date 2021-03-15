a565a87d167141ed22143a30826cc32564efa94f
VictoriaAlekseeva <77786771+VictoriaAlekseeva@users.noreply.github.com>
2021-03-15 14:59:33 +0100

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let nReverse = n.toString().split("").reverse();
+
+  if (nReverse[nReverse.length-1] == "-") return nReverse.slice(0, (nReverse.length-1)).join("");
+  else return nReverse.join("");
 }
